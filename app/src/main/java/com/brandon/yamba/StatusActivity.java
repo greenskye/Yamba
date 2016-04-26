126730ac4030b902cb54f35f7f4709667e3db08d
BHARBO3 <brandon.harbour@hallmark.com>
2016-04-26 14:42:19 -0500

    Async Task

--- app/src/main/java/com/brandon/yamba/StatusActivity.java
+++ app/src/main/java/com/brandon/yamba/StatusActivity.java
@@ -1,5 +1,6 @@
 package com.brandon.yamba;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
 
 public class StatusActivity extends AppCompatActivity {
 
@@ -27,16 +31,37 @@ public class StatusActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String status = editStatus.getText().toString();
                 Log.d(TAG, "Status to send" + status);
-//                try {
-//                    YambaClient yamba = new YambaClient("sutdent", "password");
-//                    yamba.postStatus(status);
-//                } catch (YambaClientException e) {
-//                    e.printStackTrace();
-//                }
-                Toast.makeText(StatusActivity.this, status, Toast.LENGTH_SHORT).show();
+
+                new PostTask().execute(status);
             }
         });
 
 
+    }
+
+    private final class PostTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String status = params[0];
+            String result = "Unknown Result";
+            YambaClient yambaCloud = new YambaClient("student", "password", "http://yamba.newcircle.com/api");
+            try {
+                yambaCloud.postStatus( status );
+                Log.e(TAG, "Status sent: " + status);
+                result = "Status sent: " + status;
+            } catch (YambaClientException e) {
+                Log.e(TAG, "Yamba exception: " + e.getMessage());
+                e.printStackTrace();
+                result = "Yamba exception: " + e.getMessage();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Toast.makeText(StatusActivity.this, result, Toast.LENGTH_SHORT).show();
+        }
     }
 }
