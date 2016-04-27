7c0a2b7e93ffd7e9f8de6f0d474f5d266cb6674c
BHARBO3 <brandon.harbour@hallmark.com>
2016-04-27 09:58:35 -0500

    Refactored StatusFragment to use wizard

--- app/src/main/java/com/brandon/yamba/StatusActivity.java
+++ app/src/main/java/com/brandon/yamba/StatusActivity.java
@@ -1,13 +1,18 @@
 package com.brandon.yamba;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StatusActivity extends AppCompatActivity {
+public class StatusActivity extends AppCompatActivity implements StatusFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+    }
+
+    public void onFragmentInteraction(Uri uri) {
+        // empty
     }
 }
